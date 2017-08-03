b9b64010546ffbd89ba5d3b82c26b68da18e0e4a
Nazmi Anik <nazmianik@gmail.com>
2017-08-03 15:40:43 -0400

    Finalize the Reducer

---
+++ reducerA1.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+import sys
+
+hitsTotal = 0
+oldKey = None
+
+# Loop around the data
+# It will be in the format key\tval
+# Where key is the page address, val is the hit number
+
+# All the hits for a particular page will be presented,
+# Then the key will change and we'll deal with the next item
+
+for line in sys.stdin:
+    data_mapped = line.strip().split("\t")
+    if len(data_mapped) != 2:
+        # Something has gone wrong. Skip this line.
+        continue
+
+    thisKey, thisCount = data_mapped
+
+    if oldKey and oldKey != thisKey:
+        print oldKey, "\t", hitsTotal
+        oldKey = thisKey;
+        hitsTotal = 0
+
+    oldKey = thisKey
+    hitsTotal += float(thisCount)
+
+if oldKey != None:
+    print oldKey, "\t", hitsTotal
